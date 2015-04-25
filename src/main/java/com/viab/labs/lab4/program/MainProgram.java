be519a62fc329e620e0d1e816542b102d53eaa52
Viktor <viab92@outlook.com>
2015-04-25 20:12:50 +0300

    Lab4 - new testclass

---
+++ src/main/java/com/viab/labs/lab4/program/MainProgram.java
@@ -0,0 +1,10 @@
+package com.viab.labs.lab4.program;
+
+/**
+ * Created by Viktor on 25.04.2015.
+ */
+public class MainProgram {
+    public static void main(String[] args) {
+        System.out.printf("This is for new branch");
+    }
+}
